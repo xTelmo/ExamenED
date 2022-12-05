c4e48d1b8d308029bb0e1e1fcd9e1c8449d21509
telmo <telmoparedes19@gmail.com>
2022-12-05 17:33:26 +0100

    pasando arrays

--- src/Clase.java
+++ src/Clase.java
@@ -9,6 +9,6 @@ class Clase {
 
     @Override
     public String toString() {
-        return "Objeto { i=" + entero + ", a='" + string + "'}";
+        return "Objeto { i=" + entero + ", a='" + string + "\'}";
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -22,7 +24,38 @@ public class Main {
         modificaContenidoObjeto(objeto2); // pasamos la referencia al objeto
         System.out.println("Valor del objeto fuera del método: " + objeto2);
 
+
+
+        System.out.println("\n--- Pasando arrays");
+
+        int[] array1 = new int[5];
+        printArray(array1);
+        array1[0] = 0;  // innecesario ya que inicializa a 0 por defecto
+
+        int[] array2 = array1;  // dos variables apuntando al mismo array
+        int[] array3 = Arrays.copyOf(array1, array1.length); // dos arrays iguales
+
+        IncrementaArray(array1);
+        IncrementaArray(array2);
+        IncrementaArray(array3);
+
+        printArray(array1);
+        printArray(array2);
+        printArray(array3);
     }
+
+    private static void IncrementaArray(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            array[i] = array[i] + 1;
+        }
+    }
+
+
+    private static void printArray(int[] array) {
+        Arrays.stream(array).forEach(System.out::print);
+        System.out.println();
+    }
+
 
     private static void modificaVarInt(int a) {
         a++; // Incrementamos en 1
